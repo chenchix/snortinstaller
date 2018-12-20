6ba43b86c03cc90668df6c90d5e1334868bbfc21
chencho <smunoz@caviumnetworks.com>
2018-12-20 16:34:30 +0100

    Bump snort to verion 2.9.11.1 Added packaging option to generate a tar.gz with a release package

--- snorter.sh
+++ snorter.sh
@@ -50,29 +50,27 @@ function snort_install() {
 	#Downloading SNORT
 	cd $WORKDIR && mkdir -p snort_src && cd snort_src
 	echo -ne "\n\t${CYAN}[i] INFO:${NOCOLOR} Downloading ${BOLD}SNORT${NOCOLOR}.\n\n"
-	wget --no-check-certificate -P $WORKDIR/snort_src https://snort.org/downloads/archive/snort/snort-2.9.8.2.tar.gz
+	wget --no-check-certificate -P $WORKDIR/snort_src https://snort.org/downloads/archive/snort/snort-2.9.11.1.tar.gz
 
 	#Installing SNORT
 	cd $WORKDIR/snort_src
 	echo -ne "\n\t${CYAN}[i] INFO:${NOCOLOR} Installing ${BOLD}SNORT${NOCOLOR}.\n\n"
-	tar xf snort-2.9.8.2.tar.gz
+	tar xf snort-2.9.11.1.tar.gz
 	rm -rf *.tar.gz 
 
 	#Patching snort to use hyperscan
-	cd snort-2.9.8.2
+	cd snort-2.9.11.1
 
 	patch_snort
 	
 	arch=$(uname -m)
-
-	#            --with-intel-hyperscan-libraries=$WORKDIR/hyperscan_src/release/lib \
-
 	if [ "$arch" == "aarch64" ]; then
-	        ./configure --enable-sourcefire --enable-intel-hyperscan \
-        	    --with-intel-hyperscan-includes=$WORKDIR/../prerelease/src \
+	        ./configure --enable-intel-hyperscan \
+       		    --with-intel-hyperscan-includes=$WORKDIR/../prerelease/src \
 	            --with-intel-hyperscan-libraries=$WORKDIR/../prerelease/lib \
 		    	--prefix=${RELEASE} \
-	        && make -j18 && make install
+			--enable-gre --enable-mpls --enable-targetbased --enable-ppm --enable-perfprofiling --enable-zlib --enable-active-response --enable-normalizer --enable-reload --enable-react --enable-flexresp3 \
+	        && make -j8 && make install
 	else
 		./configure --enable-sourcefire --enable-intel-hyperscan \
 	           --with-intel-hyperscan-includes=$WORKDIR/hyperscan_src/hyperscan-5.0.0/src \
@@ -90,10 +88,7 @@ function snort_install() {
 function patch_snort(){
 	
 	echo -ne "\n\t${CYAN}[i] INFO:${NOCOLOR} Downloading ${BOLD}SNORT PATCH TO USE HYPERSCAN${NOCOLOR}.\n\n"
-	#wget --no-check-certificate -P $WORKDIR/snort_src https://01.org/sites/default/files/downloads//snort-2982-hyperscan-v1.tar.gz
-	cd $WORKDIR/snort_src/snort-2.9.8.2
-	#tar xf $WORKDIR/snort_src/snort-2982-hyperscan-v1.tar.gz && rm $WORKDIR/snort_src/snort-2982-hyperscan-v1.tar.gz
-   # 	zcat snort-2982-hyperscan-v1/snort-2982-hyperscan-v1.patch.gz | patch -p2
+	cd $WORKDIR/snort_src/snort-2.9.11.1
    	patch -p2 < ${WORKDIR}/../patches/snort-2982-hyperscan-v1.patch 
 	autoreconf -fi
 }
@@ -108,7 +103,7 @@ function setup_snort(){
 	mkdir -p ${RELEASE}/var/log/snort
 	mkdir -p ${RELEASE}/etc
 
-	cd $WORKDIR/snort_src/snort-2.9.8.2
+	cd $WORKDIR/snort_src/snort-2.9.11.1
 	find -name '*.so' -exec cp {\} ${RELEASE}/lib/snort_dynamicengine/ \; 	
 	cd $HOMEDIR
 	tar xf config.tar.gz && rsync -av etc ${RELEASE}
@@ -118,10 +113,28 @@ function setup_snort(){
 
 }
 
+function setup_hyperscan(){
+	
+	echo -ne "\n\t${CYAN}[i] INFO:${NOCOLOR} Preparing hyperscan release ${NOCOLOR}.\n\n"
+	cp -rf $WORKDIR/../prerelease/bin/hsbench $RELEASE/bin/
+	cp -rf $WORKDIR/../prerelease/bin/pcapscan $RELEASE/bin/
+	cp -rf $WORKDIR/../prerelease/docs	$RELEASE/
+	cp -rf $WORKDIR/../prerelease/tools	$RELEASE/
+	cp -rf $WORKDIR/../prerelease/README.md	$RELEASE/
+	cp -rf $WORKDIR/../prerelease/*tar.bz2	$RELEASE/
+
+}
+
+function packaging(){
+	cd $HOMEDIR
+	tar cjf snort_hyperscan_marvell.tar.bz2 $RELEASE
+	
+}
 
 rm -rf $WORKDIR $RELEASE
 mkdir -p $WORKDIR
-mkdir -p $RELEASE
+mkdir -p ${RELEASE}/bin
+
 install_dependencies
 hyperscan_install
 if [ $RETURN == -1 ]; then
@@ -130,9 +143,11 @@ if [ $RETURN == -1 ]; then
 else
 	snort_install
 	setup_snort
+	setup_hyperscan
 	echo -ne "\n\t${CYAN}[i] INFO:${NOCOLOR} Run snort using this line ${NOCOLOR}.\n\n"
-	echo -ne "\n\t${BOLD} sudo LD_LIBRARY_PATH=$WORKDIR/lib $WORKDIR/bin/snort -c $WORKDIR/etc/snort.conf ${NOCOLOR}\n"
+	echo -ne "\n\t${BOLD} sudo LD_LIBRARY_PATH=$RELEASE/lib $RELEASE/bin/snort -c $RELEASE/etc/snort.conf ${NOCOLOR}\n"
 fi
 
+packaging
 rm -rf $WORKDIR/../prerelease
 rm -rf $WORKDIR
